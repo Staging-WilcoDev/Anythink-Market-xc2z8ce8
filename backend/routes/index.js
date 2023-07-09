c0ec4484feb986060b13e727b6a8b086de537648
WilcoApp <hello@wilco.gg>
2023-07-09 16:03:50 +0300

    The unhealthy health-check

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,13 +1,10 @@
-var router = require('express').Router();
+var router = require("express").Router();
 
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   res.send("Anythink backend is up.");
 });
 
-router.get('/health', (req, res, next) => {
-  res.sendStatus("200");
-})
-
-router.use('/api', require('./api'));
+router.use("/health", require("./health"));
+router.use("/api", require("./api"));
 
 module.exports = router;
