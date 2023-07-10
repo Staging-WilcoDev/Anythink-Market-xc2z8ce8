e86bd1ff8508b3d1ac5ec420d6693166ad02bf17
Erik Sapir <esapir@gmail.com>
2023-07-10 16:45:41 +0000

    ga

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ router.get("/", (req, res, next) => {
   res.send("Anythink backend is up.");
 });
 
-router.use("/health", require("./health"));
+router.use("/health2", require("./health"));
 router.use("/api", require("./api"));
 
 module.exports = router;
